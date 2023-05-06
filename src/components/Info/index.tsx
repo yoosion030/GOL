92c9253ccf018b42d21dd3753736628025dab679
yoosion030 <yoosioff@gmail.com>
2023-05-06 12:45:53 +0900

    :sparkles: Add Modal mode style
    modal mode마다 스타일 적용

--- src/components/Info/index.tsx
+++ src/components/Info/index.tsx
@@ -3,18 +3,36 @@ import { SummonersInfo } from 'types/Info';
 import InfoBanner from './InfoBanner';
 import { GameHistory, Modal } from 'components';
 import { useState } from 'react';
+import { palette } from 'shared/styles/Palette';
 
 const Info = ({ nickname, name }: SummonersInfo) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isRegisterModalOpen, setIsRegisterModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const router = useRouter();
   console.log(router);
 
   return (
     <>
       <InfoBanner nickname={nickname} name={name} />
-      <div onClick={() => setIsOpen(true)}>사용자 등록 테스트</div>
-      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)} />
-      <div>사용자 등록 해제 테스트</div>
+      <div onClick={() => setIsRegisterModalOpen(true)}>사용자 등록 테스트</div>
+      <Modal
+        isOpen={isRegisterModalOpen}
+        onClose={() => setIsRegisterModalOpen(false)}
+        modalTitle="본인의 계정이 확실한가요?"
+        modalContent={`본인의 계정이 아닌 다른 사람의 계정을 \n등록하지 않도록 주의해주세요.`}
+        mode="등록"
+        colorScheme="main"
+      />
+
+      <div onClick={() => setIsDeleteModalOpen(true)}>사용자 해제 테스트</div>
+      <Modal
+        isOpen={isDeleteModalOpen}
+        onClose={() => setIsDeleteModalOpen(false)}
+        modalTitle="정말 등록을 해제할까요?"
+        modalContent={`등록 해제 시 모든 기록이 삭제됩니다. \n정말로 등록 해제를 진행하시겠습니까? \n이 작업은 되돌릴 수 없습니다.`}
+        mode="해제"
+        colorScheme="red"
+      />
       <GameHistory />
     </>
   );
--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -7,14 +7,23 @@ import {
   Text,
 } from '@chakra-ui/react';
 import { palette } from 'shared/styles/Palette';
+interface ModalPropsType {
+  isOpen: boolean;
+  onClose: () => void;
+  modalTitle: string;
+  modalContent: string;
+  mode: string;
+  colorScheme: 'red' | 'main';
+}
 
 const ModalContainer = ({
   isOpen,
   onClose,
-}: {
-  isOpen: boolean;
-  onClose: () => void;
-}) => {
+  modalTitle,
+  modalContent,
+  mode,
+  colorScheme,
+}: ModalPropsType) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose} isCentered>
       <ModalOverlay backgroundColor="rgba(0, 0, 0, 0.24)" />
@@ -29,23 +38,24 @@ const ModalContainer = ({
         justifyContent="space-between"
       >
         <Text fontSize={palette.fontSize.medium} fontWeight="700">
-          본인의 계정이 확실한가요?
+          {modalTitle}
         </Text>
         <Text
           color={palette.color.gray}
           fontSize={palette.fontSize.smallTitle}
           lineHeight="25px"
+          whiteSpace="pre-wrap"
         >
-          본인의 계정이 아닌 다른 사람의 계정을 <br /> 등록하지 않도록
-          주의해주세요.
+          {modalContent}
         </Text>
         <Flex justifyContent="space-between" height="4.5vh">
           <Button
             width="45%"
             height="100%"
             fontSize={palette.fontSize.smallTitle}
-            color={palette.color.main}
+            color={palette.color[colorScheme]}
             onClick={onClose}
+            borderRadius="10px"
           >
             취소
           </Button>
@@ -54,9 +64,11 @@ const ModalContainer = ({
             height="100%"
             fontSize={palette.fontSize.smallTitle}
             color="#ffffff"
-            backgroundColor={palette.color.main}
+            backgroundColor={palette.color[colorScheme]}
+            borderRadius="10px"
+            colorScheme={colorScheme === 'main' ? 'blue' : colorScheme}
           >
-            등록
+            {mode}
           </Button>
         </Flex>
       </ModalContent>
