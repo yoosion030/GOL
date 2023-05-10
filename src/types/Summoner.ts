0db00a0f715929419e06b0bff78b4f138730b2f3
yoosion030 <yoosioff@gmail.com>
2023-05-10 17:16:55 +0900

    :label: Add SummonerType

---
+++ src/types/Summoner.ts
@@ -0,0 +1,25 @@
+export interface SummeonerType {
+  id: string;
+  summonerApiId: string;
+  accountId: string;
+  puuid: string;
+  name: string;
+  profileIconId: number;
+  revisionDate: number;
+  summonerLevel: number;
+  isRegistered: boolean;
+  userDto: UserDto;
+}
+
+export interface UserDto {
+  id: string;
+  email: string;
+  name: string;
+  grade: number;
+  classNum: number;
+  num: number;
+  gender: string;
+  profileUrl: string;
+  gAuthRole: string;
+  role: string;
+}
