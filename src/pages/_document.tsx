a85e49c9fb33a7d0b61f2c70aaa014d5f6c8aba9
yoosion030 <yoosioff@gmail.com>
2023-05-18 10:09:56 +0900

    :memo: Delete favicon.ico in document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,7 +20,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;600;700;900&display=swap"
             rel="stylesheet"
           />
-          <link rel="icon" href="/favicon.png" />
         </Head>
         <body>
           <Main />
