396c415ee2e1bd386cfc508ae7d24101b69e9534
yoosion030 <yoosioff@gmail.com>
2023-05-22 09:30:30 +0900

    :memo: Update BASE_URL

--- src/config/Interceptor.ts
+++ src/config/Interceptor.ts
@@ -3,5 +3,5 @@ import axios from 'axios';
 // Axios 인스턴스 생성
 export const instance = axios.create({
   // Axios 구성 옵션 설정
-  baseURL: process.env.BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
