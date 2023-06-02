b5c343f0351f8d286cb297fb718cf3c7fa2f774c
yoosion030 <yoosioff@gmail.com>
2023-06-02 14:03:45 +0900

    :bug: Add snowball spell

--- src/utils/format/formatSpellImage.ts
+++ src/utils/format/formatSpellImage.ts
@@ -1,29 +1,29 @@
-import { ImageURL } from 'shared/Image';
+import { ImageURL, ImageVersion } from 'shared/Image';
 
 const formatSpellImage = (spellId: string): string => {
-  const version = '11.14.1'; // Specify the desired Data Dragon version
-
   switch (spellId) {
     case '1':
-      return `${ImageURL}/${version}/img/spell/SummonerBoost.png`;
+      return `${ImageURL}/${ImageVersion}/img/spell/SummonerBoost.png`;
     case '3':
-      return `${ImageURL}/${version}/img/spell/SummonerExhaust.png`;
+      return `${ImageURL}/${ImageVersion}/img/spell/SummonerExhaust.png`;
     case '4':
-      return `${ImageURL}/${version}/img/spell/SummonerFlash.png`;
+      return `${ImageURL}/${ImageVersion}/img/spell/SummonerFlash.png`;
     case '6':
-      return `${ImageURL}/${version}/img/spell/SummonerHaste.png`;
+      return `${ImageURL}/${ImageVersion}/img/spell/SummonerHaste.png`;
     case '7':
-      return `${ImageURL}/${version}/img/spell/SummonerHeal.png`;
+      return `${ImageURL}/${ImageVersion}/img/spell/SummonerHeal.png`;
     case '11':
-      return `${ImageURL}/${version}/img/spell/SummonerSmite.png`;
+      return `${ImageURL}/${ImageVersion}/img/spell/SummonerSmite.png`;
     case '12':
-      return `${ImageURL}/${version}/img/spell/SummonerTeleport.png`;
+      return `${ImageURL}/${ImageVersion}/img/spell/SummonerTeleport.png`;
     case '13':
-      return `${ImageURL}/${version}/img/spell/SummonerMana.png`;
+      return `${ImageURL}/${ImageVersion}/img/spell/SummonerMana.png`;
     case '14':
-      return `${ImageURL}/${version}/img/spell/SummonerDot.png`;
+      return `${ImageURL}/${ImageVersion}/img/spell/SummonerDot.png`;
     case '21':
-      return `${ImageURL}/${version}/img/spell/SummonerBarrier.png`;
+      return `${ImageURL}/${ImageVersion}/img/spell/SummonerBarrier.png`;
+    case '32':
+      return `${ImageURL}/${ImageVersion}/img/spell/SummonerSnowball.png`;
     default:
       return '';
   }
